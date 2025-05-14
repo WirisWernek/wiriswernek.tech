9defc5c6aa10280dfb2b46476740c26c1a99d325
Wiris Wernek <wiriswernek@gmail.com>
2025-05-14 16:12:51 -0300

    feat: adiciona efeito na visualização dos projetos

--- src/app/service/projetos.service.ts
+++ src/app/service/projetos.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
 import { ProjetoModel } from '../models/projeto.model';
 
 @Injectable({
@@ -8,11 +7,11 @@ import { ProjetoModel } from '../models/projeto.model';
 export class ProjetosService {
   constructor() {}
 
-  getProjects(): Observable<ProjetoModel[]> {
-    return of([
+  getProjects(): ProjetoModel[] {
+    return [
       {
         nome: 'Cartão de Visitas',
-        descricao: 'O futuro dos ultrapassados Cartões de Visita',
+        descricao: 'O futuro dos ultrapassados Cartões de Visita.',
         tags: ['Angular', 'Typescript', 'Bootstrap', 'PWA'],
         repositorio:
           'https://github.com/WirisWernek/digital-business-card-frontend',
@@ -21,7 +20,7 @@ export class ProjetosService {
       },
       {
         nome: 'Previsão do Tempo',
-        descricao: 'Previsão do tempo rápida e simples',
+        descricao: 'Previsão do tempo rápida e simples.',
         tags: ['Angular', 'Typescript', 'Tailwind'],
         repositorio: 'https://github.com/WirisWernek/previsao_tempo',
         url: 'https://previsao-tempo-wiriswernek.vercel.app',
@@ -35,6 +34,61 @@ export class ProjetosService {
         url: 'http://cronolog.com.br/',
         preview: '/assets/images/projeto-3.svg',
       },
-    ]);
+      {
+        nome: 'Pede Aí',
+        descricao: 'Peça seu açai com um clique.',
+        tags: ['Angular', 'Bootstrap', 'WhasApp'],
+        repositorio: 'https://github.com/WirisWernek/pede-ai',
+        url: 'https://pedeai-wiriswernek.vercel.app/acai',
+        preview: '/assets/images/projeto-4.svg',
+      },
+      {
+        nome: 'Fisctrack Web',
+        descricao: 'Cliente do gerenciador de notas fiscais.',
+        tags: ['Angular', 'PrimeNg', 'Typescript'],
+        repositorio: 'https://github.com/WirisWernek/fisctrack-web',
+        preview: '/assets/images/projeto-5.svg',
+      },
+      {
+        nome: 'Fisctrack Core',
+        descricao: 'API para gerenciar notas fiscais.',
+        tags: ['Quarkus', 'Java', 'PostgreSQL'],
+        repositorio: 'https://github.com/WirisWernek/fisctrack-core',
+        url: 'https://wiriswernek.github.io/fisctrack-core',
+        preview: '/assets/images/projeto-6.svg',
+      },
+      {
+        nome: 'Github Repos',
+        descricao: 'Busque o que realmente te importa no Github.',
+        tags: ['Angular', 'Github API', 'Tailwind'],
+        repositorio: 'https://github.com/WirisWernek/github-repos',
+        url: 'https://github-repos-wiriswernek.vercel.app',
+        preview: '/assets/images/projeto-7.svg',
+      },
+      {
+        nome: 'QR Code Generator',
+        descricao: 'Gerador de QR Code simples e rápido.',
+        tags: ['Angular', 'Typescript', 'Bootstrap'],
+        repositorio: 'https://github.com/WirisWernek/qr-code-generator',
+        url: 'https://qr-code-generator-wiriswernek.vercel.app',
+        preview: '/assets/images/projeto-8.svg',
+      },
+      {
+        nome: 'Logmine',
+        descricao: 'Gerenciador de logs para aplicações Java.',
+        tags: ['Angular', 'Typescript', 'SCSS'],
+        repositorio: 'https://github.com/WirisWernek/logmine',
+        url: 'https://logmine.vercel.app/',
+        preview: '/assets/images/projeto-9.svg',
+      },
+      {
+        nome: 'Links',
+        descricao: 'Meu agregador de links.',
+        tags: ['HTML', 'CSS', 'Javascript'],
+        repositorio: 'https://github.com/WirisWernek/wiriswernek.links',
+        url: 'https://links.wiriswernek.tech/',
+        preview: '/assets/images/projeto-10.svg',
+      },
+    ];
   }
 }
--- src/app/views/home/components/projetos-section/projetos-section.component.ts
+++ src/app/views/home/components/projetos-section/projetos-section.component.ts
@@ -1,20 +1,18 @@
-import { AsyncPipe } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ProjetoModel } from '../../../../models/projeto.model';
 import { ProjetosService } from '../../../../service/projetos.service';
 import { SectionTitleComponent } from '../../../shared/components/section-title/section-title.component';
-import { ProjectCardComponent } from "./components/project-card/project-card.component";
+import { ProjectCardComponent } from './components/project-card/project-card.component';
 
 @Component({
   selector: 'app-projetos-section',
-  imports: [SectionTitleComponent, AsyncPipe, ProjectCardComponent],
+  imports: [SectionTitleComponent, ProjectCardComponent],
   templateUrl: './projetos-section.component.html',
   styleUrl: './projetos-section.component.scss',
 })
 export class ProjetosSectionComponent implements OnInit {
   private readonly projectService = inject(ProjetosService);
-  projetos!: Observable<ProjetoModel[]>;
+  projetos: ProjetoModel[] = [];
 
   ngOnInit(): void {
     this.projetos = this.projectService.getProjects();
